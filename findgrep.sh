d457a460e7a03a35b3b9e83b9cfc8480dae72b25
Shoma Miyayama <n20027@std.it-college.ac.jp>
2020-06-24 11:53:36 +0900

    file pattern

--- findgrep.sh
+++ findgrep.sh
@@ -2,9 +2,14 @@
 
 pattern=$1
 directory=$2
+name=$3
 if [ -z "$directory" ];then
 	directory='.'
 fi
+if [ -z "$name" ]; then
+	name='*'
+fi
 
-
+# -n : print line number
+# -H : print the file name
 find . -type f | xargs grep -nH "pattern"
